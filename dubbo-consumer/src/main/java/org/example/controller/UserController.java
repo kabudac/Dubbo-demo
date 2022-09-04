a7511fbda6be636d5ed15446945a8556be1d517b
kabubuda <2822797738@qq.com>
2022-09-04 17:39:08 +0800

    更新到Dubbo3

--- dubbo-consumer/src/main/java/org/example/controller/UserController.java
+++ dubbo-consumer/src/main/java/org/example/controller/UserController.java
@@ -1,9 +1,8 @@
 package org.example.controller;
 
 import com.example.service.UserService;
+import org.apache.dubbo.common.stream.StreamObserver;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.apache.dubbo.config.annotation.Method;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +20,51 @@ public class UserController {
     @RequestMapping("/say")
     public String sayHello() {
         return userService.sayHello();
+    }
+
+    @RequestMapping("/server")
+    public String server() {
+        userService.sayHelloStream("hello", new StreamObserver<String>() {
+            @Override
+            public void onNext(String data) {
+                System.out.println("接受到结果"+data);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("接受到错误:"+throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("---结束---");
+        }
+        });
+
+        return "ok";
+    }
+
+    @RequestMapping("/client")
+    public String client() {
+        StreamObserver<String> serverStreamObserver = userService.sayHelloStream(new StreamObserver<String>() {
+            @Override
+            public void onNext(String data) {
+                System.out.println("接收到"+data);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("---结束---");
+            }
+        });
+
+        serverStreamObserver.onNext("1");
+        serverStreamObserver.onNext("2");
+        serverStreamObserver.onCompleted();
+        return "ok";
     }
 }
--- dubbo-interface/src/main/java/com/example/service/UserService.java
+++ dubbo-interface/src/main/java/com/example/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import org.apache.dubbo.common.stream.StreamObserver;
+
 /**
  * @Author: kabudac
  * @Date: 2022/7/10 15:56
@@ -7,4 +9,14 @@ package com.example.service;
  */
 public interface UserService {
     String sayHello();
+
+
+    // Server Stream
+    default void sayHelloStream(String name, StreamObserver<String> response){
+    }
+
+    // Client Stream
+    default StreamObserver<String> sayHelloStream(StreamObserver<String> response){
+        return response;
+    }
 }
